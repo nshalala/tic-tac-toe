635321335da7137db19492963529a0e83bd9a7c2
Shalala <nasirlishalala1011@gmail.com>
2023-08-09 19:51:23 +0400

    modified

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ start();
 function start() {
     clearTimeout(gameOverAlert);
     while (player1 == null || player1.trim() == "") {
-        player1 = prompt("Player1:");
+        player1 = prompt("Player 1:");
     }
     while (player2 == null || player2.trim() == "") {
-        player2 = prompt("Player2:");
+        player2 = prompt("Player 2:");
     }
     turn = true;
-    tdValues = [[], [], []];
-    display(player1,player2);
+    tdValues = [];
+    display(player1, player2);
 }
 
 function display(player1, player2) {
@@ -28,13 +28,13 @@ function display(player1, player2) {
         code += `<tr>`;
         for (let j = 0; j < 3; j++) {
             code += `<td id="c${i}${j}" onclick="setValue(${i}, ${j})"></td>`;
-            tdValues[3 * i + j] = undefined;
+            tdValues[3 * i + j] = "";
         }
         code += `</tr>`;
     }
     board.innerHTML = code;
-    info.innerHTML=`Player1: <span>${player1}</span><br/>
-                    Player2: <span>${player2}</span>`;
+    info.innerHTML = `P 1: <span>${player1}</span><br/>
+                    P 2: <span>${player2}</span>`;
 }
 
 function setValue(i, j) {
@@ -48,32 +48,27 @@ function setValue(i, j) {
 }
 
 function check() {
-    if (!tdValues.includes(undefined)) gameOver("draw");
     for (let i = 0; i < 3; i++) {
         //check vertically
-        if (tdValues[i] != undefined && tdValues[i] == tdValues[i + 3] && tdValues[i + 3] == tdValues[i + 6]) {
+        if (tdValues[i] != "" && tdValues[i] == tdValues[i + 3] && tdValues[i + 3] == tdValues[i + 6]) {
             gameOver("win");
             return;
         }
         //check horizontally
-        if (
-            tdValues[3 * i] != undefined &&
-            tdValues[3 * i] == tdValues[3 * i + 1] &&
-            tdValues[3 * i + 1] == tdValues[3 * i + 2]
-        ) {
+        if (tdValues[3 * i] != "" && tdValues[3 * i] == tdValues[3 * i + 1] && tdValues[3 * i + 1] == tdValues[3 * i + 2]) {
             gameOver("win");
             return;
         }
     }
     //check diagonally
     if (
-        tdValues[4] != undefined &&
-        ((tdValues[0] == tdValues[4] && tdValues[4] == tdValues[8]) ||
-            (tdValues[2] == tdValues[4] && tdValues[4] == tdValues[6]))
+        tdValues[4] != "" &&
+        ((tdValues[0] == tdValues[4] && tdValues[4] == tdValues[8]) || (tdValues[2] == tdValues[4] && tdValues[4] == tdValues[6]))
     ) {
         gameOver("win");
         return;
     }
+    if (!tdValues.includes("")) gameOver("draw");
 }
 
 function gameOver(status) {
