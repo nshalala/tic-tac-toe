06bb2724b0c62fe53864e62169826db6ba307527
Shalala <nasirlishalala1011@gmail.com>
2023-08-09 20:40:06 +0400

    scoresAdded

--- script.js
+++ script.js
@@ -6,23 +6,24 @@ let tdValues;
 let gameOverAlert;
 let player1;
 let player2;
+let scores;
 
 start();
 
 function start() {
-    clearTimeout(gameOverAlert);
+    player1 = "";
+    player2 = "";
     while (player1 == null || player1.trim() == "") {
         player1 = prompt("Player 1:");
     }
     while (player2 == null || player2.trim() == "") {
         player2 = prompt("Player 2:");
     }
-    turn = true;
-    tdValues = [];
-    display(player1, player2);
+    scores = [0,0];
+    resetBoard();
 }
 
-function display(player1, player2) {
+function displayBoard(){
     let code = "";
     for (let i = 0; i < 3; i++) {
         code += `<tr>`;
@@ -33,8 +34,12 @@ function display(player1, player2) {
         code += `</tr>`;
     }
     board.innerHTML = code;
-    info.innerHTML = `P 1: <span>${player1}</span><br/>
-                    P 2: <span>${player2}</span>`;
+}
+
+function display(player1, player2) {
+    displayBoard();
+    info.innerHTML = `<p>${player1}: <span>${scores[0]}</span></p>
+                    <p>${player2}: <span>${scores[1]}</span></p>`;
 }
 
 function setValue(i, j) {
@@ -73,12 +78,29 @@ function check() {
 
 function gameOver(status) {
     let message = "";
+    if(turn) scores[1] += 1;
+    else scores[0] += 1;
 
-    if (status == "win") message = `${turn ? player2 : player1} won! Would you like to play again?`;
-    else message = `It is a draw :( Would you like to play again?`;
+    info.innerHTML = `<p>${player1}: <span>${scores[0]}</span></p>
+                    <p>${player2}: <span>${scores[1]}</span></p>`;
+    
+    if (status == "win") message = `${turn ? player2 : player1} won! Would you like to continue?`;
+    else message = `It is a draw :( Would you like to continue?`;
 
     gameOverAlert = setTimeout(() => {
         let again = confirm(message);
-        again ? start() : alert("Goodbye for now!");
+        again ? resetBoard() : alert(`Results:\n${player1}:  ${scores[0]}\n${player2}:  ${scores[1]}`);
     }, 100);
 }
+
+function resetBoard(){
+    clearTimeout(gameOverAlert);
+    turn = true;
+    tdValues = [];
+    display(player1, player2);
+}
+
+document.querySelector('#restart button').addEventListener("click",f=>{
+    let isSure = confirm("Are you sure to restart? You can revert this!");
+    if(isSure) start()
+})
